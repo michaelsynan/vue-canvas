ef581b770ad0fab6bdf80e45363a2b0146ac2053
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-03-10 21:37:18 -0500

    1 file modified

--- src/components/CanvasGrid.vue
+++ src/components/CanvasGrid.vue
@@ -28,23 +28,28 @@ export default {
     const canvas = this.$refs.canvas;
     const ctx = canvas.getContext('2d');
 
-    // Wait for the image to load
-    this.element.onload = () => {
-      // Set the width and height of the canvas element to match the size of the image
+    if (this.element.complete) {
+      // If the image has already loaded, set the width and height of the canvas element
       canvas.width = this.element.width;
       canvas.height = this.element.height;
+    } else {
+      // If the image hasn't loaded yet, wait for it to load before setting the width and height of the canvas element
+      this.element.onload = () => {
+        canvas.width = this.element.width;
+        canvas.height = this.element.height;
+      };
+    }
 
-      const rectWidth = canvas.width / this.columns;
-      const rectHeight = canvas.height / this.rows;
+    const rectWidth = canvas.width / this.columns;
+    const rectHeight = canvas.height / this.rows;
 
-      for (let row = 0; row < this.rows; row++) {
-        for (let col = 0; col < this.columns; col++) {
-          const x = col * rectWidth;
-          const y = row * rectHeight;
-          ctx.strokeRect(x, y, rectWidth, rectHeight);
-        }
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.columns; col++) {
+        const x = col * rectWidth;
+        const y = row * rectHeight;
+        ctx.strokeRect(x, y, rectWidth, rectHeight);
       }
-    };
+    }
   },
 };
 </script>
