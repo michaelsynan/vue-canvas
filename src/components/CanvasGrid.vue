98159630a8fd40d701dbd02feb53f511db818474
michaelsynan <93357198+michaelsynan@users.noreply.github.com>
2023-03-12 13:36:45 -0400

    1 file modified, 1 deleted

--- src/components/CanvasGrid.vue
+++ src/components/CanvasGrid.vue
@@ -1,11 +1,17 @@
 <template>
-  <canvas ref="canvas" />
+  <div>
+    <canvas ref="canvas" :width="canvasWidth" :height="canvasHeight" />
+  </div>
 </template>
 
 <script>
 export default {
   name: 'CanvasGrid',
   props: {
+    element: {
+      type: Object,
+      default: null,
+    },
     rows: {
       type: Number,
       default: 10,
@@ -15,21 +21,35 @@ export default {
       default: 10,
     },
   },
+  data() {
+    return {
+      canvasWidth: 0,
+      canvasHeight: 0,
+    };
+  },
   mounted() {
-    const canvas = this.$refs.canvas;
-    const context = canvas.getContext('2d');
+    const image = new Image();
+    image.src = this.element.src;
+    image.addEventListener('load', () => {
+      this.canvasWidth = image.width;
+      this.canvasHeight = image.height;
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext('2d');
 
-    const cellWidth = canvas.width / this.columns;
-    const cellHeight = canvas.height / this.rows;
+      const cellWidth = canvas.width / this.columns;
+      const cellHeight = canvas.height / this.rows;
 
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.columns; col++) {
-        const x = col * cellWidth;
-        const y = row * cellHeight;
+      context.drawImage(image, 0, 0, this.canvasWidth, this.canvasHeight);
 
-        context.strokeRect(x, y, cellWidth, cellHeight);
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.columns; col++) {
+          const x = col * cellWidth;
+          const y = row * cellHeight;
+
+          context.strokeRect(x, y, cellWidth, cellHeight);
+        }
       }
-    }
+    });
   },
 };
 </script>
